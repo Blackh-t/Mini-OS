eefe4e6a641f98f3149436d386bf266555b2bcea
blackh-t <nache7383@uit.no>
2024-12-19 16:48:31 +0100

    [SetUp] Bare-Metal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+/// Overwrite the Entry Point, cuz there is no underlying OS.
+/// Hardware must be initialized manually.
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
 }
